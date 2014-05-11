2145b5c1a98272c0d38b8ed4cd636816b10ffac7
Shane Sveller <shanesveller@gmail.com>
2014-05-11 16:59:00 -0500

    Reconfigure app

--- app.rb
+++ app.rb
@@ -7,17 +7,27 @@ require 'pdfkit'
 require 'uri'
 
 PDFKit.configure do |config|
-  config.wkhtmltopdf = '/usr/local/bin/wkhtmltopdf'
+  config.default_options = {
+    :page_size => 'Letter',
+    :load_error_handling => 'ignore',
+    :print_media_type => true
+  }
   config.verbose = true
 end
+
+set :bind, '0.0.0.0'
 
 get '/pdf' do
   raw_url = params[:url]
   if raw_url && URI.parse(raw_url)
     url = URI.unescape raw_url
     content_type 'application/pdf' # escaped URL for filename
-    PDFKit.new(url, :page_size => 'Letter', :load_error_handling => 'ignore').to_pdf
+    PDFKit.new(url).to_pdf
   else
     444
   end
+end
+
+get '/' do
+  "Working!"
 end
